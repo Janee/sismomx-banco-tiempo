77878f2899e3cd4db84d11f717aefa2a60485d68
Diana Enriquez <diana@conekta.com>
2017-09-24 21:10:56 -0500

    Add devise gem to repo

--- app/assets/javascripts/components/event_application.js.jsx
+++ app/assets/javascripts/components/event_application.js.jsx
@@ -4,7 +4,10 @@ var EventApplication = React.createClass({
       <div className="container">
         <div className="jumbotron">
           <h1>#BancoDeAyuda</h1>
-          <p>#BancoDeAyuda pretende ser una herramienta útil para intercambiar servicios por algún tipo de asistencia para aquellos que se hayan quedado sin trabajo y sin hogar.</p>
+          <p>
+            #BancoDeAyuda pretende ser una herramienta útil para intercambiar servicios
+            por algún tipo de asistencia para aquellos que se hayan quedado sin trabajo y sin hogar.
+          </p>
         </div>
         <div className="row">
           <div className="col-md-12">
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,4 +53,7 @@ Rails.application.configure do
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   config.react.variant = :development
+
+  # Default URL options for the Devise mailer 
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
